69b87e70eb22a3e4ee53d879266bc51c1cda9d36
Michael Davis <mcarsondavis@gmail.com>
2023-01-31 13:04:35 -0600

    Tune default configuration options
    This commit is specific to this driver branch.
    
    Why change the defaults in the code and not just use a config file?
    I end up running a lot of PRs through `cargo run` when reviewing and
    old branches might not have these options.
    
    The same could be accomplished by creating a wrapper shell script
    around `hx` that calls `hx -c /path/to/custom-config.toml $@` but
    it's more work to put that together than to just change these values
    in the code.

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -973,7 +973,7 @@ impl Default for Config {
             default_yank_register: '"',
             auto_save: AutoSave::default(),
             idle_timeout: Duration::from_millis(250),
-            completion_timeout: Duration::from_millis(250),
+            completion_timeout: Duration::from_millis(150),
             preview_completion_insert: true,
             completion_trigger_len: 2,
             auto_info: true,
@@ -991,7 +991,7 @@ impl Default for Config {
             indent_guides: IndentGuidesConfig::default(),
             color_modes: false,
             soft_wrap: SoftWrap {
-                enable: Some(false),
+                enable: Some(true),
                 ..SoftWrap::default()
             },
             text_width: 80,
@@ -1006,8 +1006,11 @@ impl Default for Config {
             popup_border: PopupBorderConfig::None,
             indent_heuristic: IndentationHeuristic::default(),
             jump_label_alphabet: ('a'..='z').collect(),
-            inline_diagnostics: InlineDiagnosticsConfig::default(),
-            end_of_line_diagnostics: DiagnosticFilter::Disable,
+            inline_diagnostics: InlineDiagnosticsConfig {
+                cursor_line: DiagnosticFilter::Enable(Severity::Error),
+                ..Default::default()
+            },
+            end_of_line_diagnostics: DiagnosticFilter::Enable(Severity::Hint),
             clipboard_provider: ClipboardProvider::default(),
         }
     }
