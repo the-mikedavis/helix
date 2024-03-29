ea6a75178450a21e14594e69e49f0e5b3762415c
Michael Davis <mcarsondavis@gmail.com>
2024-03-29 11:14:50 -0400

    WIP: shrink selection within the input range

--- helix-core/src/object.rs
+++ helix-core/src/object.rs
@@ -1,37 +1,33 @@
 use crate::{syntax::TreeCursor, Range, RopeSlice, Selection, Syntax};
 
 pub fn expand_selection(syntax: &Syntax, text: RopeSlice, selection: Selection) -> Selection {
-    let cursor = &mut syntax.walk();
-
-    selection.transform(|range| {
-        let from = text.char_to_byte(range.from());
-        let to = text.char_to_byte(range.to());
-
-        let byte_range = from..to;
-        cursor.reset_to_byte_range(from, to);
-
+    select_node_impl(syntax, text, selection, |cursor, byte_range| {
         while cursor.node().byte_range() == byte_range {
             if !cursor.goto_parent() {
                 break;
             }
         }
-
-        let node = cursor.node();
-        let from = text.byte_to_char(node.start_byte());
-        let to = text.byte_to_char(node.end_byte());
-
-        Range::new(to, from).with_direction(range.direction())
     })
 }
 
 pub fn shrink_selection(syntax: &Syntax, text: RopeSlice, selection: Selection) -> Selection {
-    select_node_impl(syntax, text, selection, |cursor| {
+    select_node_impl(syntax, text, selection, |cursor, byte_range| {
         cursor.goto_first_child();
+        while cursor.node().start_byte() < byte_range.start
+            || cursor.node().end_byte() > byte_range.end
+        {
+            if !cursor.goto_next_sibling() {
+                // If a child within the range couldn't be found, default to the first child.
+                cursor.goto_parent();
+                cursor.goto_first_child();
+                break;
+            }
+        }
     })
 }
 
 pub fn select_next_sibling(syntax: &Syntax, text: RopeSlice, selection: Selection) -> Selection {
-    select_node_impl(syntax, text, selection, |cursor| {
+    select_node_impl(syntax, text, selection, |cursor, _byte_range| {
         while !cursor.goto_next_sibling() {
             if !cursor.goto_parent() {
                 break;
@@ -41,7 +37,7 @@ pub fn select_next_sibling(syntax: &Syntax, text: RopeSlice, selection: Selectio
 }
 
 pub fn select_prev_sibling(syntax: &Syntax, text: RopeSlice, selection: Selection) -> Selection {
-    select_node_impl(syntax, text, selection, |cursor| {
+    select_node_impl(syntax, text, selection, |cursor, _byte_range| {
         while !cursor.goto_prev_sibling() {
             if !cursor.goto_parent() {
                 break;
@@ -57,7 +53,8 @@ fn select_node_impl<F>(
     motion: F,
 ) -> Selection
 where
-    F: Fn(&mut TreeCursor),
+    // Fn(tree cursor, original selection's byte range)
+    F: Fn(&mut TreeCursor, std::ops::Range<usize>),
 {
     let cursor = &mut syntax.walk();
 
@@ -67,7 +64,7 @@ where
 
         cursor.reset_to_byte_range(from, to);
 
-        motion(cursor);
+        motion(cursor, from..to);
 
         let node = cursor.node();
         let from = text.byte_to_char(node.start_byte());
