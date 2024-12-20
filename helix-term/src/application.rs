511adf558682a4b65b1b94b6129a97d09ffdf8b1
Michael Davis <mcarsondavis@gmail.com>
2024-12-20 13:35:59 -0500

    (11889) Replace url::Url with a String wrapper

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -730,7 +730,7 @@ impl Application {
                         }
                     }
                     Notification::PublishDiagnostics(params) => {
-                        let uri = match helix_core::Uri::try_from(params.uri) {
+                        let uri = match helix_core::Uri::try_from(params.uri.as_str()) {
                             Ok(uri) => uri,
                             Err(err) => {
                                 log::error!("{err}");
@@ -1047,7 +1047,8 @@ impl Application {
             ..
         } = params
         {
-            self.jobs.callback(crate::open_external_url_callback(uri));
+            self.jobs
+                .callback(crate::open_external_url_callback(uri.as_str()));
             return lsp::ShowDocumentResult { success: true };
         };
 
@@ -1058,7 +1059,7 @@ impl Application {
             ..
         } = params;
 
-        let uri = match helix_core::Uri::try_from(uri) {
+        let uri = match helix_core::Uri::try_from(uri.as_str()) {
             Ok(uri) => uri,
             Err(err) => {
                 log::error!("{err}");
